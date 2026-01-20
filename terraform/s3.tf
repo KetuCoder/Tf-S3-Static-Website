b2dfc2a0dea72713ff5cf08c0521753b5e53da61
KetuCoder <ambureketan@gmail.com>
2026-01-20 07:52:25 +0530

    update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,12 +1,18 @@
 resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
-
-  acl           = "public-read"
+  bucket        = var.bucket_name
   force_destroy = true
 
   tags = {
     Name        = "Terraform Static Website"
     Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.static_site.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
@@ -25,19 +31,20 @@ resource "aws_s3_bucket_website_configuration" "website" {
   index_document {
     suffix = "index.html"
   }
-
-  error_document {
-    key = "error.html"
-  }
 }
 
 resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.static_site.id
 
+  depends_on = [
+    aws_s3_bucket_public_access_block.allow_public
+  ]
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
